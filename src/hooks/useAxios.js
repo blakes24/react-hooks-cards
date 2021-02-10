6cac41a278ea6a14be10ed0f390b18ba79e70d6f
blakes24 <blakes24@gmail.com>
2021-02-10 11:54:59 -0800

    Add useAxios to PokeDex

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -2,11 +2,12 @@ import { useState } from 'react';
 import uuid from 'uuid';
 import axios from 'axios';
 
-const useAxios = (url) => {
+const useAxios = (baseUrl) => {
 	const [ items, setItems ] = useState([]);
 
-	async function addItem() {
-		const response = await axios.get(url);
+	async function addItem(appendUrl) {
+		const addUrl = typeof appendUrl !== 'object' ? appendUrl : '';
+		const response = await axios.get(baseUrl + addUrl);
 		setItems((items) => [ ...items, { ...response.data, id: uuid() } ]);
 	}
 	return [ items, addItem ];
